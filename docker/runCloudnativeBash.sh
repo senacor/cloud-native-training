2fc3082e886ae50c2e2b476dc5cd42a6e1c9c414
Scholz, Richard <7677934+nextstepman@users.noreply.github.com>
2018-11-08 22:52:33 +0100

    fix script

--- docker/runCloudnativeBash.sh
+++ docker/runCloudnativeBash.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it -v ${pwd}:/porject -v $HOME/.aws:/root/.aws:ro -v $HOME/.ssh:/root/.ssh:ro -v $HOME/.kube:/root/.kube  cloudnative_bash:latest
+docker run -it -v $(pwd):/project -v $HOME/.minikube:$HOME/.minikube -v $HOME/.aws:/root/.aws:ro -v $HOME/.ssh:/root/.ssh:ro -v $HOME/.kube:/root/.kube  cloudnative_bash:latest
