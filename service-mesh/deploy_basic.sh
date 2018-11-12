a2ad83dc786fd5091ca54d4c34201608c0ef583f
Scholz, Richard <7677934+nextstepman@users.noreply.github.com>
2018-11-12 11:04:09 +0100

    back to no inject and no global auth

--- service-mesh/deploy_basic.sh
+++ service-mesh/deploy_basic.sh
@@ -1,6 +1,6 @@
 # setup the namespace with automatic istio
 kubectl create namespace sock-shop
-kubectl label namespace sock-shop istio-injection=enabled
+# kubectl label namespace sock-shop istio-injection=enabled
 kubectl config set-context minikube --namespace=sock-shop
 
 kubectl apply -f front-end-gateway.yaml
