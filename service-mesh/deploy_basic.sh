519f30948cc10545c21be1c0e0377ff8cf97b6bc
Scholz, Richard <7677934+nextstepman@users.noreply.github.com>
2018-11-12 10:12:32 +0100

    fix gateway

--- service-mesh/deploy_basic.sh
+++ service-mesh/deploy_basic.sh
@@ -3,5 +3,5 @@ kubectl create namespace sock-shop
 kubectl label namespace sock-shop istio-injection=enabled
 kubectl config set-context minikube --namespace=sock-shop
 
-kubectl apply -f catalogue-virtual-service.yaml
+kubectl apply -f front-end-gateway.yaml
 kubectl apply -f rbac-permissive.yaml
